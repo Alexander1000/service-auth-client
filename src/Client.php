ee93eb5c2df9b8ee3eb04224f15d3939c555d774
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2019-03-30 23:56:38 +0300

    update

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,25 @@
 
 namespace Alexander1000\Clients\Auth;
 
+use NetworkTransport;
+
 class Client
 {
+    /**
+     * @var NetworkTransport\Http\Transport
+     */
+    private $transport;
+
+    /**
+     * @var NetworkTransport\Http\Request\Builder
+     */
+    private $requestBuilder;
+
+    public function __construct(
+        NetworkTransport\Http\Transport $transport,
+        NetworkTransport\Http\Request\Builder $requestBuilder
+    ) {
+        $this->transport = $transport;
+        $this->requestBuilder = $requestBuilder;
+    }
 }
